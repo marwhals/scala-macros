861abc8fe42df1b7b4ccd6365bef628e10607af6
marwhals <marjan.mubarok2012@gmail.com>
2025-05-26 16:31:36 +0100

    Quote Matching - Pattern matching on expressions

---
+++ src/main/scala/macros_usage/QuoteMatching.scala
@@ -0,0 +1,18 @@
+package macros_usage
+
+import macros.QuoteMatching.*
+
+object QuoteMatching {
+  val a = pmOptions(Some(42))
+  val b = pmOptions(None)
+  val c = pmOptions(Option(42)) // Option(42) != Some(42) as an EXPRESSION
+  val d = pmOptions(new Some(42)) // new Some(42) != Some(42) as an EXPRESSION
+  val e = pmOptions(Some(2 + 5)) // the expression Some(2 + 5) != Some(7)
+  val f = pmGeneric(Some(45))
+  val h = pmAny(Some(List(1,2,3)))
+  val i = pmErasureAvoidance(List(1, 2, 4))
+  val j = pmErasureAvoidance(List("Scala", "macros"))
+  val k = pmErasureAvoidance(List('a', 'b', 'c'))
+  val l = pmListExpression(List(1, 2, 3).map(_.toString).map(_.length))
+  val m = pmListExpression(List("Scala", "is", "insane").map(_.length).map(_.toString))
+}
