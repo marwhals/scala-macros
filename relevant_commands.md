68746aa00b054b33180c2aa10f05c1918866f094
marwhals <marjan.mubarok2012@gmail.com>
2025-06-01 20:02:14 +0100

    Code optimizer: Collection calls

---
+++ relevant_commands.md
@@ -0,0 +1,5 @@
+# Useful commands for this repo
+
+
+### Redirect compiler output to a text file.
+```sbt compile > compile-output.txt 2>&1```
---
+++ src/main/scala/wartimzer/WartimizerUsage.scala
@@ -0,0 +1,51 @@
+package wartimzer
+
+import Wartimizer.wartimize
+
+/**
+* - Uncomment to see custom scala compile errors from macros
+*/
+
+
+case class Person(name: String, email: String)
+
+object WartimizerUsage {
+
+  // normal scala code
+  val badPractice = "This is Scala: " + Person("John Cena", "jonSeena@wweDaBest.com")
+
+  // Can put this into an SBT plugin
+  // This does not compile ---> which is what we want!!!!
+//  val linted = wartimize(StringPlusAny)("This is Scala: " + Person("John Cena", "jonSeena@wweDaBest.com"))
+
+  // This does not compile because the variable is not a compile-time known singleton instance (object)
+//  val wartimizer: Wartimization = StringPlusAny
+//  val linted = wartimize(wartimizer)("This is Scala: " + Person("John Cena", "jonSeena@wweDaBest.com"))
+
+  // validated code - should just compile just fine
+  val stringConcatenation = wartimize(StringPlusAny)("Scala is " + "Tasty")
+
+  val firstEven = List(1,2,3,4,5).filter(_ %  2 == 0).headOption
+  // rewritten to list.find(_ % 2 == 0)
+//   val firstEven_v2 = wartimize(CollectionOptimizer)(List(1,2,3,4,5).filter(_ % 2 == 0).headOption)
+
+  // does not compile because of StringPlusAny - good!  ---- can combo linters and optimizers 🔥
+  // val combinedWartimized = wartimize(StringPlusAny, CollectionOptimizer)(List(1,2,3,4,5).filter(_ % 2 == 0).headOption.map("Scala " + _))
+
+  // Map.get.getOrElse
+  val simpleMap = Map("Alice" -> 123, "Bob" -> 456, "Charlie" -> 789)
+  val personNumber = simpleMap.get("Martin").getOrElse(999)
+  val personNumber_v2 = wartimize(CollectionOptimizer)(simpleMap.get("Martin").getOrElse(999))
+
+  // successive map transformation
+  val mappedList = List(1,2,3).map(_ + 1).map(_ * 3)
+  val optimizedList = wartimize(CollectionOptimizer)(List(1, 2, 3).map(_ + 1).map(_ * 3).map(_.toString + " Scala is great"))
+
+  val combinedCalls = List(1, 2, 3).map(_ + 1).map(_ * 3).map(_.toString + " Scala is great").filter(_.length > 5).headOption
+  val optimizedCombinedCalls = wartimize(CollectionOptimizer)(List(1, 2, 3).map(_ + 1).map(_ * 3).map(_.toString + " Scala is great").filter(_.length > 5).headOption)
+
+  def main(a: Array[String]) = {
+    println(badPractice)
+  }
+
+}
